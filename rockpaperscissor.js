6cf7ed6f2046953800f36a5d3fe86a5f964d1f64
Christin John <42737168+christhelonewolf@users.noreply.github.com>
2020-04-11 20:20:03 +0530

    upload 1

---
+++ rockpaperscissor.js
@@ -0,0 +1,42 @@
+//class  play is the class that creates randon values for the player 
+//the value is returned in 
+class play{
+
+ fun () {
+
+const inte = Math.floor(Math.random()*3)
+if(inte === 0 ){
+    return 0 //stone
+}else if(inte === 1){
+    return 1 // scissor
+}else{
+    return 2 //paper
+}
+}
+
+}
+
+const point = 0
+const player1 = new play()
+const player2 = new play()
+const player3 = new play()
+const player4 = new play()
+
+const player1choice = player1.fun()
+const player2choice = player2.fun()
+const player3choice = player3.fun()
+const player4choice = player4.fun()
+
+
+// if (player1choice === 1){
+//     constchoice ==
+// }
+if(0<1 ){
+    point = 1
+}
+if(0)
+
+console.log(player1choice)
+console.log(player2choice)
+console.log(player3choice)
+console.log(player4choice)
