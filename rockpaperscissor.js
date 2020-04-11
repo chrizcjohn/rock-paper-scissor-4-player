f470264b94e34942dac4fd980705abf290827264
Christin John <42737168+christhelonewolf@users.noreply.github.com>
2020-04-11 20:40:48 +0530

    Update rockpaperscissor.js

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,27 +1,29 @@
 //class  play is the class that creates randon values for the player 
-//the value is returned in 
-class play{
-
- fun () {
-
-const inte = Math.floor(Math.random()*3)
-if(inte === 0 ){
-    return 0 //stone
-}else if(inte === 1){
-    return 1 // scissor
-}else{
-    return 2 //paper
-}
-}
+class play {
+    //This function is used to create a random value that is 0(stone) or 1(scissor) or 2(paper) 
+    fun() {
+        const inte = Math.floor(Math.random() * 3)
+        if (inte === 0) {
+            return 0 // return 0 which is stone
+        } else if (inte === 1) {
+            return 1 // return 1 which is scissor
+        } else {
+            return 2 // return 2 which is paper
+        }
+    }
 
 }
 
 const point = 0
-const player1 = new play()
+
+//four objects of class play which act as four players
+const player1 = new play() 
 const player2 = new play()
 const player3 = new play()
 const player4 = new play()
 
+//four choice which take returned value of fun funtions which will be the
+//random choices the 4 players will be making
 const player1choice = player1.fun()
 const player2choice = player2.fun()
 const player3choice = player3.fun()
@@ -31,10 +33,6 @@ const player4choice = player4.fun()
 // if (player1choice === 1){
 //     constchoice ==
 // }
-if(0<1 ){
-    point = 1
-}
-if(0)
 
 console.log(player1choice)
 console.log(player2choice)
